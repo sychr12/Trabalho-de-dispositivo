a9293dbb1a04b996d75818a280acfe68f93e4d6c
Elikson Tavares <Eliksonbt.eks@gmail.com>
2025-10-28 21:07:50 -0400

    feat: split profile login in times screen

---
+++ components/times/professorTimes.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const ProfessorTimes = () => {
+  return (
+    <View>
+      <Text>professorTimes</Text>
+    </View>
+  );
+};
+
+export default ProfessorTimes;
---
+++ components/times/studentTimes.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const StudentTimes = () => {
+  return (
+    <View>
+      <Text>StudentTimes</Text>
+    </View>
+  );
+};
+
+export default StudentTimes;
